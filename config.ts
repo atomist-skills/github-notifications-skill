4cc50012eb7a20537dd04c5ae925e4e1f0ce9e41
Christian Dupuis <cd@atomist.com>
2020-01-11 00:02:35 +0100

    Initial commit

---
+++ config.ts
@@ -0,0 +1,50 @@
+import {
+    ConfigurationMaker,
+    YamlCommandHandlerRegistration,
+} from "@atomist/sdm-core/lib/machine/yaml/configureYaml";
+import { mapCommand } from "@atomist/sdm-core/lib/machine/yaml/mapCommand";
+import { githubLifecycleSupport } from "@atomist/sdm-pack-lifecycle-github";
+import { ExtensionPack } from "@atomist/sdm/lib/api/machine/ExtensionPack";
+import { SoftwareDeliveryMachine } from "@atomist/sdm/lib/api/machine/SoftwareDeliveryMachine";
+import { CommandHandlerRegistration } from "@atomist/sdm/lib/api/registration/CommandHandlerRegistration";
+import * as _ from "lodash";
+
+export const LifecycleConfig: ConfigurationMaker = async cfg => {
+    const gls = githubLifecycleSupport();
+    const functionLifecycleSupport: ExtensionPack = {
+        ...gls,
+        configure: sdm => {
+
+            const proxy = new Proxy<SoftwareDeliveryMachine>(sdm, {
+                get: (target, propKey) => {
+                    if (propKey === "addCommand") {
+                        return (...args) => {
+                            const cmd = args[0] as CommandHandlerRegistration;
+                            target[propKey]({ name: cmd.name, ...mapCommand(cmd)(sdm) as YamlCommandHandlerRegistration });
+                        };
+                    } else {
+                        return target[propKey];
+                    }
+                },
+            });
+            gls.configure(proxy);
+
+            /*const cmds = [];
+            sdm.commandHandlers.forEach(c => {
+                const md = metadataFromInstance(toFactory(c)()) as CommandHandlerMetadata;
+                if (!!md.intent && md.intent.length > 0) {
+                    cmds.push({
+                        name: md.name,
+                        description: md.description,
+                        intent: toArray(md.intent)[0],
+                    });
+                }
+            });
+            logger.info("\n" + JSON.stringify({ commands: cmds }, undefined, 2));*/
+        },
+    };
+
+    _.set(cfg, "sdm.extensionPacks", [functionLifecycleSupport]);
+
+    return cfg as any;
+};
