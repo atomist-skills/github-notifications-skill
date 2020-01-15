4f27b2dfb70e235e37ce67f3ae1c744a8af64ad4
Christian Dupuis <cd@atomist.com>
2020-01-15 22:49:29 +0100

    Update to latest dependencies

--- config.ts
+++ config.ts
@@ -1,58 +1,32 @@
-import {
-    ConfigurationMaker,
-    YamlCommandHandlerRegistration,
-} from "@atomist/sdm-core/lib/machine/yaml/configureYaml";
-import { mapCommand } from "@atomist/sdm-core/lib/machine/yaml/mapCommand";
+import { ConfigurationMaker } from "@atomist/sdm-core/lib/machine/yaml/configureYaml";
 import { githubLifecycleSupport } from "@atomist/sdm-pack-lifecycle-github";
-import { ExtensionPack } from "@atomist/sdm/lib/api/machine/ExtensionPack";
-import { SoftwareDeliveryMachine } from "@atomist/sdm/lib/api/machine/SoftwareDeliveryMachine";
-import { CommandHandlerRegistration } from "@atomist/sdm/lib/api/registration/CommandHandlerRegistration";
 import * as _ from "lodash";
 
 export const LifecycleConfig: ConfigurationMaker = async cfg => {
-    const gls = githubLifecycleSupport();
-    const functionLifecycleSupport: ExtensionPack = {
-        ...gls,
-        configure: sdm => {
-            // TODO cd this could move to sdm/sdm-core
-            const proxy = new Proxy<SoftwareDeliveryMachine>(sdm, {
-                get: (target, propKey) => {
-                    if (propKey === "addCommand") {
-                        return (...args) => {
-                            const cmd = args[0] as CommandHandlerRegistration;
-                            target[propKey]({ name: cmd.name, ...mapCommand(cmd)(sdm) as YamlCommandHandlerRegistration });
-                        };
-                    } else {
-                        return target[propKey];
-                    }
-                },
-            });
-            gls.configure(proxy);
 
-            // Uncomment to populate atomist.yaml
-            /*const cmds = [];
-            sdm.commandHandlers.forEach(c => {
-                const md = metadataFromInstance(toFactory(c)()) as CommandHandlerMetadata;
-                if (!!md.intent && md.intent.length > 0) {
-                    cmds.push({
-                        name: md.name,
-                        description: md.description,
-                        intent: toArray(md.intent)[0],
-                    });
-                }
+    // Uncomment to populate atomist.yaml
+    /** const cmds = [];
+    sdm.commandHandlers.forEach(c => {
+        const md = metadataFromInstance(toFactory(c)()) as CommandHandlerMetadata;
+        md.intent.forEach(i => {
+            cmds.push({
+                name: md.name,
+                description: md.description,
+                intent: i,
             });
-            logger.info("\n" + JSON.stringify({ commands: cmds }, undefined, 2));
-            const events = [];
-            sdm.eventHandlers.forEach(c => {
+        });
+    });
+    logger.info("\n" + JSON.stringify({ commands: cmds }, undefined, 2));*/
+    /**
+     const events = [];
+     sdm.eventHandlers.forEach(c => {
+                const md = metadataFromInstance(toFactory(c)()) as EventHandlerMetadata;
                 if (!!md.subscription) {
                     events.push(md.subscription);
                 }
             });
-            logger.info("\n" + JSON.stringify({ subscriptions: events }, undefined, 2));*/
-        },
-    };
-
-    _.set(cfg, "sdm.extensionPacks", [functionLifecycleSupport]);
+     logger.info("\n" + JSON.stringify({ subscriptions: events }, undefined, 2));*/
+    _.set(cfg, "sdm.extensionPacks", [githubLifecycleSupport()]);
 
     return cfg as any;
 };
