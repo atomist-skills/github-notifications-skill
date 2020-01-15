afc73233eb569a908b6638f0b20194858643a68b
Christian Dupuis <cd@atomist.com>
2020-01-16 00:01:51 +0100

    Update intents

--- config.ts
+++ config.ts
@@ -3,30 +3,35 @@ import { githubLifecycleSupport } from "@atomist/sdm-pack-lifecycle-github";
 import * as _ from "lodash";
 
 export const LifecycleConfig: ConfigurationMaker = async cfg => {
-
-    // Uncomment to populate atomist.yaml
-    /** const cmds = [];
-    sdm.commandHandlers.forEach(c => {
-        const md = metadataFromInstance(toFactory(c)()) as CommandHandlerMetadata;
-        md.intent.forEach(i => {
-            cmds.push({
-                name: md.name,
-                description: md.description,
-                intent: i,
-            });
-        });
-    });
-    logger.info("\n" + JSON.stringify({ commands: cmds }, undefined, 2));*/
-    /**
-     const events = [];
-     sdm.eventHandlers.forEach(c => {
+        /*const printCommandsExtensionPack: ExtensionPack = {
+            ...metadata("print-commands"),
+            configure: sdm => {
+                // Uncomment to populate atomist.yaml
+                const cmds = [];
+                sdm.commandHandlers.forEach(c => {
+                    const md = metadataFromInstance(toFactory(c)()) as CommandHandlerMetadata;
+                    if (!!md.intent && md.intent.length > 0) {
+                        cmds.push({
+                            name: md.name,
+                            description: md.description,
+                            pattern: md.intent[0],
+                        });
+                    }
+                });
+                logger.info("\n" + JSON.stringify({ commands: cmds }, undefined, 2));
+                 const events = [];
+                 sdm.eventHandlers.forEach(c => {
                 const md = metadataFromInstance(toFactory(c)()) as EventHandlerMetadata;
                 if (!!md.subscription) {
                     events.push(md.subscription);
                 }
             });
-     logger.info("\n" + JSON.stringify({ subscriptions: events }, undefined, 2));*/
-    _.set(cfg, "sdm.extensionPacks", [githubLifecycleSupport()]);
+                 logger.info("\n" + JSON.stringify({ subscriptions: events }, undefined, 2));
+            },
+        };*/
 
-    return cfg as any;
-};
+        _.set(cfg, "sdm.extensionPacks", [githubLifecycleSupport()]);
+
+        return cfg as any;
+    }
+;
