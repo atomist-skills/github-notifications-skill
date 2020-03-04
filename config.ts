7e87ac1564f3dc3d950e5159abbc912e4ab10efd
Christian Dupuis <cd@atomist.com>
2020-03-04 13:48:41 +0100

    Polish

--- config.ts
+++ config.ts
@@ -3,37 +3,36 @@ import { ConfigurationMaker } from "@atomist/sdm/lib/core/machine/yaml/configure
 import * as _ from "lodash";
 
 export const LifecycleConfig: ConfigurationMaker = async cfg => {
-        /*
-        const printCommandsExtensionPack: ExtensionPack = {
-            ...metadata("print-commands"),
-            configure: sdm => {
-                // Uncomment to populate atomist.yaml
-                const cmds = [];
-                sdm.commandHandlers.forEach(c => {
-                    const md = metadataFromInstance(toFactory(c)()) as CommandHandlerMetadata;
-                    if (!!md.intent && md.intent.length > 0) {
-                        cmds.push({
-                            name: md.name,
-                            description: md.description,
-                            pattern: md.intent[0],
-                        });
-                    }
-                });
-                logger.info("\n" + JSON.stringify({ commands: cmds }, undefined, 2));
-                 const events = [];
-                 sdm.eventHandlers.forEach(c => {
-                const md = metadataFromInstance(toFactory(c)()) as EventHandlerMetadata;
-                if (!!md.subscription) {
-                    events.push(md.subscription);
+    /*
+    const printCommandsExtensionPack: ExtensionPack = {
+        ...metadata("print-commands"),
+        configure: sdm => {
+            // Uncomment to populate atomist.yaml
+            const cmds = [];
+            sdm.commandHandlers.forEach(c => {
+                const md = metadataFromInstance(toFactory(c)()) as CommandHandlerMetadata;
+                if (!!md.intent && md.intent.length > 0) {
+                    cmds.push({
+                        name: md.name,
+                        description: md.description,
+                        pattern: md.intent[0],
+                    });
                 }
             });
-                 logger.info("\n" + JSON.stringify({ subscriptions: events }, undefined, 2));
-            },
-        };
-        */
+            logger.info("\n" + JSON.stringify({ commands: cmds }, undefined, 2));
+             const events = [];
+             sdm.eventHandlers.forEach(c => {
+            const md = metadataFromInstance(toFactory(c)()) as EventHandlerMetadata;
+            if (!!md.subscription) {
+                events.push(md.subscription);
+            }
+        });
+             logger.info("\n" + JSON.stringify({ subscriptions: events }, undefined, 2));
+        },
+    };
+    */
 
-        _.set(cfg, "sdm.extensionPacks", [githubLifecycleSupport()]);
+    _.set(cfg, "sdm.extensionPacks", [githubLifecycleSupport()]);
 
-        return cfg as any;
-    }
-;
+    return cfg as any;
+};
