4baccf6a3f7ca215bbef8fc5055337c80c5ad477
Christian Dupuis <cd@atomist.com>
2020-05-15 13:24:39 +0200

    Add support for checks
    fixes #10

--- config.ts
+++ config.ts
@@ -1,9 +1,19 @@
-import { githubLifecycleSupport } from "@atomist/sdm-pack-lifecycle-github";
+import { metadataFromInstance } from "@atomist/automation-client/lib/internal/metadata/metadataReading";
+import {
+    CommandHandlerMetadata,
+    EventHandlerMetadata,
+} from "@atomist/automation-client/lib/metadata/automationMetadata";
+import { toFactory } from "@atomist/automation-client/lib/util/constructionUtils";
+import { logger } from "@atomist/automation-client/lib/util/logger";
+import { lifecycleSupport } from "@atomist/sdm-pack-lifecycle";
+import { DefaultGitHubLifecycleOptions } from "@atomist/sdm-pack-lifecycle-github/lib/githubLifecycleSupport";
+import { metadata } from "@atomist/sdm/lib/api-helper/misc/extensionPack";
+import { ExtensionPack } from "@atomist/sdm/lib/api/machine/ExtensionPack";
 import { ConfigurationMaker } from "@atomist/sdm/lib/core/machine/yaml/configureYaml";
 import * as _ from "lodash";
 
 export const LifecycleConfig: ConfigurationMaker = async cfg => {
-    /*
+
     const printCommandsExtensionPack: ExtensionPack = {
         ...metadata("print-commands"),
         configure: sdm => {
@@ -28,11 +38,17 @@ export const LifecycleConfig: ConfigurationMaker = async cfg => {
             }
         });
              logger.info("\n" + JSON.stringify({ subscriptions: events }, undefined, 2));
+
+             throw new Error("");
         },
     };
-    */
 
-    _.set(cfg, "sdm.extensionPacks", [githubLifecycleSupport()]);
+    const options = DefaultGitHubLifecycleOptions;
+    delete options.push.web;
+    delete options.issue.web;
+    delete options.pullRequest.web;
+
+    _.set(cfg, "sdm.extensionPacks", [lifecycleSupport(DefaultGitHubLifecycleOptions), printCommandsExtensionPack]);
 
     return cfg as any;
 };
